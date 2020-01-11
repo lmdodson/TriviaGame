9a34c9a1381976de6e5873108557da5b4380b064
Leah Dodson <leahdodson9@gmail.com>
2020-01-10 19:11:58 -0600

    changed the answer display to a for loop

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,35 +1,30 @@
 //clear
 $(document).ready(function () {
-    //click function to enter the game
+    //!click function to enter the game
     $("#game-start").click(function () {
         $("#instructions").empty();
         $("#game-start").empty();
         questionDisplay();
     });
 
-    //define game variables
+    //!define game variables
     var timerRunning = false;
     var timer = 20;
     var intervalId;
     var unanswered = 0;
 
+
     var questions = {
         question: "When firing at the enemy, where should you aim?",
-        options: [{
-            letter: "A",
-            choice: "Directly at the enemy",
-        }, {
-            letter: "B",
-            choice: "Behind the enemy in case they step back",
-        }, {
-            letter: "C",
-            choice: "At the enemy's feet so they know you mean business",
-        }, {
-            letter: "D",
-            choice: "Both B and C are correct answers"
-        }],
+        options: [
+            "A: Directly at the enemy",
+            "B: Behind the enemy in case they step back",
+            "C: At the enemy's feet so they know you mean business",
+            "D: Both B and C are correct answers"
+        ],
     };
 
+    //!Timing functions
     function runTimer() {
         if (!timerRunning) {
             intervalId = setInterval(decrement, 1000);
@@ -47,21 +42,31 @@ $(document).ready(function () {
         $("#timer").html("Time Remaining: " + timer);
         if (timer === 0) {
             unanswered++;
+            console.log(unanswered);
             stopTimer()
         }
     };
 
-
+    //!Game functions
     function questionDisplay() {
         $("#question").text(questions.question);
         console.log(questions.question);
-        $(".answer-a").text("A: " + questions.options[0].choice);
-        $(".answer-b").text("B: " + questions.options[1].choice);
-        $(".answer-c").text("C: " + questions.options[2].choice);
-        $(".answer-d").text("D: " + questions.options[3].choice);
+        //loop to display the answer options
+        for (var i = 0; i < questions.options.length; i++) {
+            var userOption = $("<div>");
+            userOption.addClass("answer");
+            userOption.html(questions.options[i]);
+            userOption.attr("position", i)
+            $("#answer-options").append(userOption)
+        }
 
-        console.log(questions.options[0].choice + "\n" + questions.options[1].choice);
         runTimer();
+
+        // $(".answer-a").text("A: " + questions.options[0].choice);
+        // $(".answer-b").text("B: " + questions.options[1].choice);
+        // $(".answer-c").text("C: " + questions.options[2].choice);
+        // $(".answer-d").text("D: " + questions.options[3].choice);
+
     }
 
 
